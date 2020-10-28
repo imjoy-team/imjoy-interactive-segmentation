ce0c9c48f22c0d2d7a06ee43105d67de9f4bc8f8
Wei Ouyang <oeway007@gmail.com>
2020-10-28 15:13:52 +0100

    remove function

--- imgseg/hpa_seg_utils.py
+++ imgseg/hpa_seg_utils.py
@@ -18,19 +18,6 @@ HIGH_THRESHOLD = 0.4
 LOW_THRESHOLD = HIGH_THRESHOLD - 0.25
 
 
-def download_with_url(url_string, file_path, unzip=False):
-    """Download file with a link."""
-    with urllib.request.urlopen(url_string) as response, open(
-        file_path, "wb"
-    ) as out_file:
-        data = response.read()  # a `bytes` object
-        out_file.write(data)
-
-    if unzip:
-        with zipfile.ZipFile(file_path, "r") as zip_ref:
-            zip_ref.extractall(os.path.dirname(file_path))
-
-
 def label_nuclei(nuclei_pred):
     """Return the labeled nuclei mask data array.
 
