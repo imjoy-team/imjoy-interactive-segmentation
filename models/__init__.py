81f2b1c9d8aedc403d9a8b5fc4dfeb93c2e2d942
oeway <oeway007@gmail.com>
2021-01-07 19:32:20 +0100

    reorganize and improve tests

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,63 @@
 class InteractiveModel:
-    def __init__(self, model_path):
+    """Interactive model interface"""
+
+    def __init__(self, model_path, *args, **kwargs):
+        """load and initialize the model"""
         pass
 
     def train_once(self, X, y):
+        """train the model for one iteration
+        Parameters
+        --------------
+        X: array [channel, width, height]
+            the input image with 2 channels
+
+        y: array [channel, width, height]
+
+
+        Returns
+        ------------------
+        None
+        """
         raise NotImplementedError
 
     def predict(self, X):
+        """predict the model for one input image
+        Parameters
+        --------------
+        X: array [channel, width, height]
+            the input image with 2 channels
+
+        Returns
+        ------------------
+        y_predict: the predicted label image
+        """
         raise NotImplementedError
 
     def save(self, model_path):
+        """save the model
+        Parameters
+        --------------
+        model_path: string
+            the file path to the model
+
+        Returns
+        ------------------
+        None
+        """
+        raise NotImplementedError
+
+    def export(self, format, file_path):
+        """export the model into different format
+        Parameters
+        --------------
+        format: string
+            the model format to be exported
+        file_path: string
+            the file path to the exported model
+
+        Returns
+        ------------------
+        None
+        """
         raise NotImplementedError
--- tests/test_cellpose.py
+++ tests/test_cellpose.py
@@ -1,11 +1,19 @@
 import os
 import sys
 
+import numpy as np
+
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 import cellpose
-from models.cellpose import CellPoseInteractiveModel
-from cellpose.utils2 import read_multi_channel_image, geojson_to_label, read_image
+from models.interactive_cellpose import CellPoseInteractiveModel
+from cellpose.utils2 import (
+    read_multi_channel_image,
+    geojson_to_label,
+    read_image,
+    load_train_test_data,
+)
 from download_example_dataset import download_with_url
 
 
@@ -18,8 +26,10 @@ if not os.path.exists(dataset_path):
 
 channels = ["er.png", "nuclei.png"]
 mask_filter = "cell_masks.png"
-folder = "./data/hpa_dataset_v2/train/3213_1239_D9_1"
+folder = "./data/hpa_dataset_v2/test/407_1852_D7_32"
 annotation_file = os.path.join(folder, "annotation.json")
+mask_filter = "cells_masks.png"
+image_filter = "er.png,nuclei.png"
 
 # create labels file
 geojson_to_label(annotation_file, save_as="_masks.png")
@@ -30,19 +40,74 @@ X = read_multi_channel_image(folder, channels, rescale=1.0)
 # read the labels
 y = read_image(folder + "/cell_masks.png", rescale=1.0)
 
-model = CellPoseInteractiveModel()
+
+model = CellPoseInteractiveModel(style_on=0, default_diameter=100)
 
 
 def test_train_once():
+    assert y.shape[1:] == (512, 512)
     model.train_once(X, y)
+
+
+def test_train():
+    output = load_train_test_data(
+        "./data/hpa_dataset_v2/train/",
+        "./data/hpa_dataset_v2/test/",
+        image_filter.split(","),
+        mask_filter,
+        unet=False,
+        rescale=1.0,
+    )
+    images, labels, image_names, test_images, test_labels, image_names_test = output
+    model.train((images, labels), (test_images, test_labels), iterations=len(images))
+
+
+def test_train_steps():
+    output = load_train_test_data(
+        "./data/hpa_dataset_v2/train/",
+        None,
+        image_filter.split(","),
+        mask_filter,
+        unet=False,
+        rescale=1.0,
+    )
+    images, labels, image_names, test_images, test_labels, image_names_test = output
+    # diams = np.array([cellpose.utils.diameters(labels[i])[0] for i in range(len(images))])
+    # mean_diam = diams.mean()
+
+    for i in range(10):
+        y_predict = model.predict(X)
+        assert y_predict.shape == (512, 512)
+        file_path = os.path.join(
+            "./data/cellpose_predicted_mask_epoch_{}.png".format(i * 100)
+        )
+        cellpose.io.imsave(file_path, y_predict)
+
+        print("Training for round {}".format(i))
+        model.train(
+            (images, labels), (test_images, test_labels), iterations=len(images) * 100
+        )
 
 
 def test_predict():
     y_predict = model.predict(X)
     assert y_predict.shape == (512, 512)
-    cellpose.io.imsave(os.path.join(folder, "cellpose_predicted_mask.png"), y_predict)
+    file_path = os.path.join("./data/cellpose_predicted_mask.png")
+    cellpose.io.imsave(file_path, y_predict)
+    assert os.path.exists(file_path)
+    # os.remove(file_path)
+
+
+def test_save():
+    model_path = "./data/cellpose_model.pth"
+    model.save(model_path)
+    assert os.path.exists(model_path)
+    # os.remove(model_path)
 
 
 if __name__ == "__main__":
-    test_train_once()
+    # test_train()
+    # test_train_steps()
     test_predict()
+    test_save()
+    test_train_once()
