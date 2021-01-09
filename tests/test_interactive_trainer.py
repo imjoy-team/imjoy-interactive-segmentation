919cddca0221e5b09269b14a28c8f77b2ebab95f
trangle1302 <lengochaitrang@gmail.com>
2021-01-09 22:00:04 +0100

    add test_workflow()

--- tests/test_interactive_trainer.py
+++ tests/test_interactive_trainer.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import time
+import shutil
 import asyncio
 import threading
 import numpy as np
+from imageio import imwrite
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
@@ -16,7 +18,9 @@ model = CellPoseInteractiveModel(
     channels=[2, 3],
     style_on=0,
     default_diameter=100,
+    pretrained_model=False,
 )
+
 trainer = InteractiveTrainer.get_instance(
     model,
     "./data/hpa_dataset_v2",
@@ -24,7 +28,7 @@ trainer = InteractiveTrainer.get_instance(
     object_name="cell",
     scale_factor=1.0,
     batch_size=1,
-    resume=True,
+    resume=False,
 )
 
 
@@ -39,7 +43,42 @@ def test_predict():
     mask = np.clip(mask > 0 * 255, 0, 255).astype("uint8")
 
 
+def test_workflow():
+    data_dir = "./data/hpa_dataset_v2"
+    val_name = "30609_1240_G3_4"
+    val_sample = trainer.get_sample("valid", val_name)
+    history = []
+    for i in range(101):
+        loss = trainer.train_once()
+        print(loss, len(trainer.sample_pool))
+        history += [loss]
+        if i % 10 == 0:
+            geojson, mask = trainer.predict(val_sample[0])
+            mask = np.clip(mask > 0 * 255, 0, 255).astype("uint8")
+            imwrite(f"./data/hpa_dataset_v2/{val_name}_epoch_{i}.png", mask)
+            _, _, info = trainer.get_test_sample()
+            # trainer.push_sample(info.name, geojson_annotation, target_folder="train")
+            sample_dir = info["path"]
+            sample_name = info["name"]
+            new_sample_dir = sample_dir.replace("test", "train")
+            shutil.move(sample_dir, new_sample_dir)
+
+            img = trainer.load_input_image("train", sample_name)
+            mask = trainer.load_target_image("train", sample_name)
+            trainer.sample_pool.append(
+                (img, mask, {"name": sample_name, "path": new_sample_dir})
+            )
+            if len(trainer.sample_pool) > trainer.max_pool_length:
+                trainer.sample_pool.pop(0)
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(range(101), history)
+    plt.savefig("./data/hpa_dataset_v2/test_history.png")
+
+
 if __name__ == "__main__":
-    test_train_once()
-    test_predict()
+    # test_train_once()
+    # test_predict()
+    test_workflow()
     os._exit(0)
