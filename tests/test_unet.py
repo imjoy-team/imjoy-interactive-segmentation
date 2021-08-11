3c9e830f283ce3ae7faa5a4a6408b6a623ee5f92
trangle1302 <lengochaitrang@gmail.com>
2021-08-11 10:37:11 +0200

    add test unet

---
+++ tests/test_unet.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import time
+import shutil
+import numpy as np
+from imageio import imread, imwrite
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
+from imjoy_interactive_trainer.interactive_trainer import InteractiveTrainer
+from imjoy_interactive_trainer.data_utils import (
+    plot_history,
+    fig2img,
+    plot_mask_overlay,
+)
+
+model_config = dict(
+    type="unet",
+    model_dir="./data/hpa_dataset_v2/__models__",
+    use_gpu=False,
+    channels=[2, 3],
+    pretrained_model=False,
+    resume=False,
+)
+print(model_config["type"])
+from imjoy_interactive_trainer.models.interactive_unet import UnetInteractiveModel
+model = UnetInteractiveModel(**model_config)
+
+trainer = InteractiveTrainer.get_instance(
+    model_config,
+    "./data/hpa_dataset_v2",
+    ["microtubules.png", "er.png", "nuclei.png"],
+    object_name="cell",
+    scale_factor=1.0,
+)
