ef24fde49e59770f3a02ae2ba0f87a4db9bd15a6
trangle1302 <lengochaitrang@gmail.com>
2021-08-18 10:27:56 +0200

    format

--- imjoy_interactive_trainer/models/interactive_unet_1.py
+++ imjoy_interactive_trainer/models/interactive_unet_1.py
@@ -8,8 +8,10 @@ from pathlib import Path
 import segmentation_models_pytorch as smp
 import albumentations as A
 
+
 def to_tensor(x, **kwargs):
-    return x.transpose(2, 0, 1).astype('float32')
+    return x.transpose(2, 0, 1).astype("float32")
+
 
 class UnetInteractiveModel:
     def __init__(
@@ -32,14 +34,14 @@ class UnetInteractiveModel:
         if use_gpu:
             if not torch.cuda.is_available():
                 print("No GPU found")
-                self.device = torch.device('cpu')
+                self.device = torch.device("cpu")
                 gpu = False
             else:
                 self.device = torch.cuda.get_device_name(0)
                 gpu = True
         else:
             gpu = False
-            self.device = torch.device('cpu')
+            self.device = torch.device("cpu")
         self.learning_rate = learning_rate
         self.channels = channels
         self.batch_size = batch_size
@@ -76,20 +78,25 @@ class UnetInteractiveModel:
         self.model.optimizer = torch.optim.Adam(
             self.model.parameters(), lr=self.learning_rate, weight_decay=1e-5
         )
-                
+
         self.loss = smp.utils.losses.DiceLoss()
         self.metrics = [
             smp.utils.metrics.IoU(threshold=0.5),
         ]
-        preprocessing_fn = smp.encoders.get_preprocessing_fn(backbone, pretrained='imagenet')
-        self.augmentator = A.Compose([
-                                A.ShiftScaleRotate(),
-                                A.RGBShift(),
-                                A.Blur(),
-                                A.GaussNoise(),
-                                A.Lambda(image=preprocessing_fn),
-                                A.Lambda(image=to_tensor, mask=to_tensor)
-                            ],p=1)
+        preprocessing_fn = smp.encoders.get_preprocessing_fn(
+            backbone, pretrained="imagenet"
+        )
+        self.augmentator = A.Compose(
+            [
+                A.ShiftScaleRotate(),
+                A.RGBShift(),
+                A.Blur(),
+                A.GaussNoise(),
+                A.Lambda(image=preprocessing_fn),
+                A.Lambda(image=to_tensor, mask=to_tensor),
+            ],
+            p=1,
+        )
 
     def get_config(self):
         """augment the images and labels
@@ -105,12 +112,13 @@ class UnetInteractiveModel:
             2) `optimizer` the optimizer for training
             3) `loss` the loss for training
             4) `lr` the learning rate
-            5) 
+            5)
         """
-        return {"batch_size": self.batch_size,
-        "optimizer": self.model.optimizer,
-        "loss": self.loss,
-        "lr": self.learning_rate
+        return {
+            "batch_size": self.batch_size,
+            "optimizer": self.model.optimizer,
+            "loss": self.loss,
+            "lr": self.learning_rate,
         }
 
     def transform_labels(self, label_image):
@@ -126,8 +134,10 @@ class UnetInteractiveModel:
             the transformed label image
         """
         assert label_image.ndim == 3 and label_image.shape[2] == 1
-        #transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor()])
-        t_label_image = label_image.astype("float32")#.transpose(2, 0, 1).astype('float32')
+        # transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor()])
+        t_label_image = label_image.astype(
+            "float32"
+        )  # .transpose(2, 0, 1).astype('float32')
         return t_label_image
 
     def augment(self, images, labels):
@@ -145,8 +155,8 @@ class UnetInteractiveModel:
         (images, labels) both are: array [batch_size, width, height, channel]
             augmented images and labels
         """
-        #images = [images[i].transpose(2, 0, 1) for i in range(images.shape[0])]
-        #labels = [labels[i].transpose(2, 0, 1) for i in range(labels.shape[0])]
+        # images = [images[i].transpose(2, 0, 1) for i in range(images.shape[0])]
+        # labels = [labels[i].transpose(2, 0, 1) for i in range(labels.shape[0])]
 
         nimg = len(images)
         # check that arrays are correct size
@@ -161,12 +171,12 @@ class UnetInteractiveModel:
         for image, mask in zip(images, labels):
             print(image.shape, image.max(), mask.shape, mask.max())
             sample = self.augmentator(image=image, mask=mask)
-            img, lb = sample['image'], sample['mask']
+            img, lb = sample["image"], sample["mask"]
             # img = self.preprocess_input_fn(img)
             imgi += [img]
             lbl += [lb]
-        imgi = torch.from_numpy(np.asarray(imgi)).to(self.device)#.unsqueeze(0)
-        lbl = torch.from_numpy(np.asarray(lbl)).to(self.device)#.unsqueeze(0)
+        imgi = torch.from_numpy(np.asarray(imgi)).to(self.device)  # .unsqueeze(0)
+        lbl = torch.from_numpy(np.asarray(lbl)).to(self.device)  # .unsqueeze(0)
         print(f"Agument output dtype {type(imgi)}")
         return imgi, lbl
 
@@ -205,8 +215,8 @@ class UnetInteractiveModel:
         loss value
         """
         assert X.shape[0] == y.shape[0] and X.ndim == 4
-        #x_tensors = torch.from_numpy(X).to(self.device).unsqueeze(0)
-        #y_tensors = torch.from_numpy(y).to(self.device).unsqueeze(0)
+        # x_tensors = torch.from_numpy(X).to(self.device).unsqueeze(0)
+        # y_tensors = torch.from_numpy(y).to(self.device).unsqueeze(0)
         return self.train(X, y)
 
     def predict(self, X):
@@ -221,11 +231,13 @@ class UnetInteractiveModel:
         array [batch_size, width, height, channel]
             the predicted label image
         """
-        assert X.ndim == 4 # len(X.shape) == 4 #.ndim == 4
+        assert X.ndim == 4  # len(X.shape) == 4 #.ndim == 4
         print(f"Input shape {X.shape}, type {torch.from_numpy(X).dtype}, max {X.max()}")
-        x_tensor = torch.from_numpy(X.astype('float32')).permute(0,3,2,1).to(self.device)
+        x_tensor = (
+            torch.from_numpy(X.astype("float32")).permute(0, 3, 2, 1).to(self.device)
+        )
         pr_mask = self.model.predict(x_tensor)
-        pr_mask = (pr_mask.permute(0,3,2,1).cpu().numpy().round())
+        pr_mask = pr_mask.permute(0, 3, 2, 1).cpu().numpy().round()
         print(f"predicted output shape {pr_mask.shape}, max {pr_mask.max()}")
         return pr_mask
 
